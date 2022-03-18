11617143d398ff668161a7351a1a223896813e1f
jovinlidan <jovinlidan1@gmail.com>
2022-03-18 17:08:41 +0700

    fix typo

--- 12_Platform Widget/pratikum/lib/main.dart
+++ 12_Platform Widget/pratikum/lib/main.dart
@@ -5,6 +5,6 @@ import 'package:pratikum/soal2.dart';
 // Hasil Soal1 ada pada Widget Soal1 dan Hasil Soal2 ada pada Widget Soal2
 // Silahkan uncomment salah satu Soal1 / Soal2 dibawah dan lakukan hot restart pada aplikasi
 void main() {
-  // runApp(Soal1());
   runApp(Soal1());
+  // runApp(Soal2());
 }
