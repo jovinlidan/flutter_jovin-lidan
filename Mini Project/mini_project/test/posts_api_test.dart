0f7ee6b0134a8da4b3486e1ffe4282ca4967a567
jovinlidan <jovinlidan1@gmail.com>
2022-05-13 00:10:39 +0700

    finish unit testing

--- Mini Project/mini_project/test/posts_api_test.dart
+++ Mini Project/mini_project/test/posts_api_test.dart
@@ -1,15 +1,17 @@
 import 'package:mini_project/model/api/post_api.dart';
+import 'package:mini_project/model/post_model.dart';
 import 'package:mini_project/services/services.dart';
 import 'package:mini_project/view_models/posts_view_model.dart';
 import 'package:mockito/annotations.dart';
 import 'package:test/test.dart';
 
-// import 'contact_api_test.mocks.dart';
+import 'posts_api_test.mocks.dart';
+import 'package:mockito/mockito.dart';
 
 @GenerateMocks([PostAPI])
 void main() {
-  group('Contact API', () {
-    // MockContactAPI contactAPI = MockContactAPI();
+  group('Post API', () {
+    MockPostAPI postAPI = MockPostAPI();
     test('get all posts returns state', () async {
       var postViewModel = PostsViewModel();
       expect(postViewModel.posts?.status, ApiStatus.success);
@@ -21,11 +23,13 @@ void main() {
 
       expect(postViewModel.posts?.status, ApiStatus.success);
     });
-    // test('get all contacts returns data', () async {
-    //   when(contactAPI.getContacts())
-    //       .thenAnswer((_) async => <Contact>[Contact(id: 1, name: 'a', phone: '1')]);
-    //   var contacts = await contactAPI.getContacts();
-    //   expect(contacts.isNotEmpty, true);
-    // });
+    test('get all posts returns data', () async {
+      when(postAPI.getPosts()).thenAnswer((_) async => <Post>[
+            Post(comments: [], createdAt: DateTime.now(), description: "TEST", sId: "1", user: [])
+          ]);
+      var posts = await postAPI.getPosts();
+      expect(posts.isNotEmpty, true);
+      expect(posts[0].description, "TEST");
+    });
   });
 }
