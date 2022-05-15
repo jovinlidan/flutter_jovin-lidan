bebca114b2a658c05b8f058829c788b266293b99
jovinlidan <jovinlidan1@gmail.com>
2022-05-15 22:07:12 +0700

    add folder final project

--- Mini Project/mini_project/lib/constants/color.constant.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomColors {
-  static const primary1 = Color.fromRGBO(0, 29, 105, 1);
-  static const primary2 = Color.fromRGBO(142, 151, 253, 1);
-  static const primary3 = Color.fromRGBO(81, 89, 121, 1);
-  static const primary4 = Color.fromARGB(255, 17, 75, 223);
-}
